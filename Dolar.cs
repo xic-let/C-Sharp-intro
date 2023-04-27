da8a484c31bf599c7a054f2076f073a4e445ebef
xic-let <114702163+xic-let@users.noreply.github.com>
2023-04-27 16:10:56 +0100

    Convert Dolars to Euros

---
+++ Dolar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorMoeda
+{
+    internal class Dolar
+    {
+        public static double cotacao;
+        public static double Quantidade;
+        public static double Imposto = 6.0; 
+
+        public static double dolar2euro(double cotacao, double Quantidade)
+        {
+        
+            double conta = Quantidade * cotacao;
+            return conta + conta * Imposto / 100.00;
+              
+        }
+
+
+    }
+}
