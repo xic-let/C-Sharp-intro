49cba1b7387f04ba6b4c0b79d26ad5b6bed922df
xic-let <114702163+xic-let@users.noreply.github.com>
2023-04-27 16:15:58 +0100

    Calcula Area | Perimetro | Diagonal Retangulo

--- Program.cs
+++ Program.cs
@@ -1,52 +1,21 @@
-﻿using System;
-using System.Globalization;
-using triangulo;
+﻿using retangulo;
+using System;
+using System.Diagnostics;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Triangulo x, y; // declaração classe triangulo;
-        x= new Triangulo(); // instaciar os objectos da classe
-        y= new Triangulo();
-        
-        double trx, triy;
-        //double p;
+        calculos ret = new calculos();
 
-        Console.WriteLine("Triangulo x, Insira lado1:");
-        x.A = double.Parse(Console.ReadLine());
-        Console.WriteLine("Triangulo x,Insira lado2:");
-        x.B = double.Parse(Console.ReadLine());
-        Console.WriteLine("Triangulo x,Insira lado3:");
-        x.C = double.Parse(Console.ReadLine());
+        Console.WriteLine("Insira a Largura do Triangulo: ");
+        Console.WriteLine("Largura:");
+        ret.larg = double.Parse(Console.ReadLine());
+        Console.WriteLine("Insira a Altura do Triangulo: ");
+        Console.WriteLine("Altura:");
+        ret.alt = double.Parse(Console.ReadLine());
 
-        Console.WriteLine("Triangulo y, Insira lado1:");
-        y.A = double.Parse(Console.ReadLine());
-        Console.WriteLine("Triangulo y,Insira lado2:");
-        y.B = double.Parse(Console.ReadLine());
-        Console.WriteLine("Triangulo y,Insira lado3:");
-        y.C = double.Parse(Console.ReadLine());
-
-        /* = (x.A + x.B + x.C) / 2;
-         trx = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
-         p = (y.A + y.B + y.C) / 2;
-         triy = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));*/
-
-         trx = x.Area();
-         triy = y.Area();
-
-         Console.WriteLine("Área do trinagulo X mede" + trx.ToString("F2"));
-         Console.WriteLine("Área do trinagulo Y mede" + triy.ToString("F2"));
-
-
-        if (trx > triy)
-        {
-            Console.WriteLine("Triangulo X tem a maior área");
-        }
-        else
-        {
-            Console.WriteLine("Triangulo Y tem a maior área ");
-        }
+        Console.WriteLine(ret);
 
     }
 }
---
+++ calculos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace retangulo
+{
+    internal class calculos
+    {
+        public double larg, alt;
+
+        public double area()
+        {
+            return larg * alt;
+        }
+        public double perimetro()
+        {
+            return (2 * larg) + (2 * alt);
+        }
+        public double diagonal()
+        {
+            return Math.Sqrt(larg * larg) + (alt * alt);
+        }
+
+        public override string ToString()
+        {
+            return "AREA = "
+                + area().ToString("F2")
+                + Environment.NewLine
+                + "PERIMETRO = "
+                + perimetro().ToString("F2")
+                + Environment.NewLine
+                + "DIAGONAL = "
+                + diagonal().ToString("F2");
+        }
+    }
+}
