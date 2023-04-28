da2e71488804a2245514233906df882c174f1a8b
xic-let <114702163+xic-let@users.noreply.github.com>
2023-04-28 14:56:59 +0100

    Manage a Bank account

---
+++ AccountOps.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class AccountOps
+    {
+        public int NrConta;
+        public string Titular;
+        public double Saldo;
+        public double Vdep;
+        public double Vlev;
+        public static double Imposto = 5.00;
+
+        public AccountOps()
+        {
+          
+        }
+
+        public AccountOps (int Nr, string Nome, double Valor)
+        {
+            NrConta = Nr;
+            Titular = Nome;
+            Saldo = Valor;
+        }
+
+        public void depositoabertura()
+        {
+             Console.WriteLine(" Entre o valor do Depósito Inicial: ");
+             Console.WriteLine(" Valor ");
+             Saldo = double.Parse(Console.ReadLine());
+        }
+           
+        public void  levantamento()
+        {
+            Console.WriteLine(" Entre o valor a levantar: ");
+            Console.WriteLine(" Valor ");
+            Vlev = double.Parse(Console.ReadLine());
+            Saldo = Saldo - Vlev - Imposto;
+        }
+        public void deposito()
+        {
+            Console.WriteLine(" Entre o valor a depositar: ");
+            Console.WriteLine(" Valor ");
+            Vdep = double.Parse(Console.ReadLine());
+            Saldo = Saldo + Vdep;
+        }
+
+        public override string ToString()
+        {
+            return Titular.ToString()
+                +" Conta: "
+                + NrConta.ToString()
+                + " Saldo Actualizado: "
+                + Saldo.ToString("F2")
+                + " EUR.";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,47 +1,78 @@
-﻿using System;
-using stocks;
+﻿using Banco;
+using System;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+/*Num banco, para se criar uma conta bancária, 
+é necessário informar o número da conta, o nome do
+titular da conta, e o valor de depósito inicial que o titular depositou ao abrir a conta.
+Este valor de depósito inicial, entretanto, é opcional, ou seja: se o titular não tiver dinheiro
+a depositar no momento de abrir sua conta, o depósito inicial não será feito e o saldo
+inicial da conta será, naturalmente, zero.
+Importante: uma vez que uma conta bancária foi aberta, o número da conta nunca poderá ser alterado. 
+Já o nome do titular pode ser alterado (pois uma pessoa pode mudar de sobrenome por ocasião de casamento, por exemplo).
+Por fim, o saldo da conta não pode ser alterado livremente. É preciso haver um mecanismo para proteger isso. 
+O saldo só aumenta por meio de depósitos, e só diminui por meio de levantamentos. Para cada levantamento realizado,
+o banco cobra uma taxa de 5.00 euros. 
+Nota: a conta pode ficar com saldo negativo se o saldo não for suficiente para realizar o levantamento e/ou pagar a taxa.
+Você deve fazer um programa que realize a criação de uma conta, dando opção para que seja ou não
+informado o valor de depósito inicial. Em seguida, realizar um depósito
+e depois um levantamento, e mostrar sempre os dados da conta após cada operação.*/
 
-namespace stocks
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        char x;
+        int y;
+        
+        AccountOps conta = new AccountOps();
+
+        Console.WriteLine(" Entre o Numero de Conta: ");
+        Console.WriteLine(" Conta Numero: ");
+        conta.NrConta = int.Parse(Console.ReadLine());
+        Console.WriteLine(" Entre o Titular  da Conta: ");
+        Console.WriteLine(" Nome: ");
+        conta.Titular = Console.ReadLine();
+        Console.WriteLine(" Haverá depósito inicial? s/n ");
+        Console.WriteLine(" Resposta: ");
+        x = char.Parse(Console.ReadLine());
         {
-            Produto p = new Produto();
-
-
-            Console.WriteLine(" Digite o nome do Produto: ");
-            Console.WriteLine("Nome: ");
-            p.Nome = Console.ReadLine();
-
-            Console.WriteLine(" Digite o Valor do Produto: ");
-            Console.WriteLine("Valor: ");
-            p.Valor = double.Parse(Console.ReadLine());
-
-            /*Console.WriteLine(" Digite a Quantidade de Produto em Stock: ");
-            Console.WriteLine("Produtos em Stock: ");
-            p.QtS = int.Parse(Console.ReadLine());
-
-            Console.WriteLine(" Digite a Quantidade de Produto vendida: ");
-            Console.WriteLine("Produtos Vendidos: ");
-            p.QtV = int.Parse(Console.ReadLine());*/
-
-            /*p.Stock = (p.QtS - p.QtV);
-
-            Console.WriteLine("Dados de Produto:" + p.Nome +
-                "Preço: " + p.Valor + " Quantidade a adicionar ao Stock:" +
-                p.QtS + " Quantidade a remover ao Stock: " +
-                p.QtV + ". Quantidade de Stock actualizada: " + p.Stock + ".");*/
-
-            Console.WriteLine("Digite a quantidade de Produto a ser adicionada: ");
-            int quantidade = int.Parse(Console.ReadLine());
-            p.AdicionarProduto(quantidade);
-
-            Console.WriteLine("Digite a quantidade de Produto a ser removida: ");
-            quantidade = int.Parse(Console.ReadLine());
-            p.RemoverProduto(quantidade);
-
-            Console.WriteLine("Dados Actualizados:" + p);
+            if (x == 's')
+            {
+                conta.depositoabertura();
+            }
+            else
+                conta.Saldo = 0;
         }
+        Console.WriteLine(" Para mais operações seleccione: ");
+        Console.WriteLine(" 1 - Levantamento");
+        Console.WriteLine(" 2 - Depósito ");
+        Console.WriteLine(" 3 - Terminar operação");
+        Console.WriteLine(" Resposta: ");
+        y= int.Parse(Console.ReadLine());
+        {
+            if (y == 1)
+            {
+                conta.levantamento();
+            }
+            if (y == 2)
+            {
+                conta.deposito();
+            }
+            else
+            {
+                Console.WriteLine("Obrigado e Volte Sempre!");
+            }
+        }
+
+
+        //AccountOps conta = new AccountOps(int Nr, string Nome, double Valor);
+
+        Console.WriteLine(" Dados de Conta: " + conta);
+        Console.WriteLine(" +++++++++++++++++++++++++");
+
+
+
     }
 }
